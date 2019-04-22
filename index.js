9f411a0137a44a8c2783e7ec775b953322131d4c
daniakabani <daneakabane@gmail.com>
2019-04-22 11:21:33 +0800

    adding modular function call in the index file

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 // local dependencies & modules call 
 const identityLogin = require('./src/authenticator/v1/identity/index'); // authenticate login module import
 
-// identity login test
-identityLogin("post", "http://localhost:3000/api/v1/identity/login", null, {
-    "name": "testt",
-    "secret": "123456789"
-});
+module.exports = {
+    identityLogin
+}
