a968ecba71780a6c70b9a01a800554f1f4999ca3
daniakabani <daneakabane@gmail.com>
2019-04-23 14:22:39 +0800

    bug fixes

--- v1/index.js
+++ v1/index.js
@@ -2,8 +2,6 @@
 require('dotenv').config();
 const identityLogin = require('../src/authenticator/v1/identity').identityLogin; // authenticate login module import
 
-identityLogin('test', '123456789');
-
 module.exports = {
     identityLogin
 };
