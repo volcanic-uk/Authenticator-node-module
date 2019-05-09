89de0315decff438e1482121675ed8d6db46fa48
daniakabani <daneakabane@gmail.com>
2019-05-09 11:36:55 +0800

    bug fixes, and example removal

--- example/routes/logout.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const logoutMiddleWare = require('../controllers/logout').logoutMiddleWare;
-
-router.get('/', logoutMiddleWare);
-
-module.exports = router;
