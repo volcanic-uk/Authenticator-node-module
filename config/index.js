e21046c655252e997cbc8f32432eb4b108bacaca
Ali Dalal <ali.dalal92@gmail.com>
2019-10-31 16:30:14 +0800

    update env.example and modify env vars name

--- config/index.js
+++ config/index.js
@@ -12,21 +12,21 @@ module.exports = {
         enableCaching: ENV_VARS.ENABLE_CACHING
     },
     auth: {
-        identity_name: ENV_VARS.IDENTITY,
-        secret: ENV_VARS.SECRET,
-        dataset_id: ENV_VARS.DATASET_ID,
-        audience: ENV_VARS.DEFAULT_AUDIENCE || ['*'],
+        identity_name: ENV_VARS.AUTH_IDENTITY,
+        secret: ENV_VARS.AUTH_SECRET,
+        dataset_id: ENV_VARS.AUTH_DATASET_ID,
+        audience: ENV_VARS.AUTH_DEFAULT_AUDIENCE || ['*'],
         set: function (authConfig) {
             this.identity_name = authConfig.identity_name;
             this.secret = authConfig.secret;
-            this.principal_id = authConfig.principal_id;
+            this.dataset_id = authConfig.dataset_id;
             this.audience = authConfig.audience;
         },
         get: function () {
             return {
                 identity_name: this.identity_name,
                 secret: this.secret,
-                principal_id: this.principal_id,
+                dataset_id: this.dataset_id,
                 audience: this.audience
             };
         }
