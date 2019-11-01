80bd5d916686361891cfe1ee778d59136c311dd1
daniakabani <daneakabane@gmail.com>
2019-11-01 09:59:30 +0800

    some more progress

--- test/principal/02_read.js
+++ test/principal/02_read.js
@@ -1,54 +1,88 @@
 const chai = require('chai'),
     chaiAsPromised = require('chai-as-promised'),
     sorted = require('chai-sorted'),
-    axiosVCR = require('axios-vcr'),
-    expect = chai.expect;
+    expect = chai.expect,
+    nock = require('../../src/helpers/index').nock;
 chai.use(chaiAsPromised);
 chai.use(sorted);
 
 const Principal = require('../../v1').Principal;
 
-let principalID = null,
-    tempPrincipalName = 'principal-test-read',
-    tempDataSetID = '11';
-
-
 describe('Principal read', () => {
     let principal = new Principal();
-    before(async () => {
-        axiosVCR.mountCassette('./test/cassettes/main_ops/principal/principal_get_by_id_test.json');
-        let create = await principal.withAuth().create(tempPrincipalName, tempDataSetID);
-        principalID = create.id;
-        axiosVCR.ejectCassette('./test/cassettes/main_ops/principal/principal_get_by_id_test.json');
-    });
     // reading principal
     it('should not proceed if the principal does not exist upon read request, and it will throw an error', async () => {
-        axiosVCR.mountCassette('./test/cassettes/principals/read/principal_read_fail.json', true);
         try {
-            await principal.withAuth().getByID(principalID + 12);
+            nock('/identity/login', 'post', {
+                name: 'volcanic',
+                secret: 'volcanic!123',
+                dataset_id: '-1',
+                audience: '["volcanic"]'
+            }, 200, {
+                response: {
+                    response: {
+                        token: 'eyJhbGciOiJFUzUxMiIsInR5cCI6IkpXVCIsImtpZCI6IjljYjg1YTc3YTllNWU0MTU3ODMyYTFlYTgzOTI3MDZhIn0.eyJleHAiOjE1NzI0OTYzNDIsInN1YiI6InVzZXI6Ly9zYW5kYm94Ly0xLzEvMS8yIiwibmJmIjoxNTcyNDkyNzQyLCJhdWRpZW5jZSI6WyJrcmFrYXRvYWV1IiwiLSJdLCJpYXQiOjE1NzI0OTI3NDIsImlzcyI6InZvbGNhbmljX2F1dGhfc2VydmljZV9hcDIifQ.AIIsVxwqsYWg3DqusQhC8qeBbIX22Rk6fZHwY2iNgnU-ghOJDmK9QNMZbqJDul5hqTXfFyB7HVw0SBXjivPtFunDAOytU-JupKTl7qgveRiU0oVMdtrtEI7iSNXS30p2ulEu0bumUjibTEW4oig0K4LJYoNxht_rPosOx_NPqCxp1ljB'
+                    }
+                },
+                status: 200
+            });
+            let scope = nock('/principals/12', 'get', {}, 404, {
+                message: 'Principal does not exist',
+                errorCode: 2002
+            });
+            await principal.withAuth().getByID(12);
+            scope.done();
             throw 'can not retrieve a principal that does not exist';
         } catch (e) {
             expect(e.message).equals('Principal does not exist');
         }
-        axiosVCR.ejectCassette('./test/cassettes/principals/read/principal_read_fail.json');
     });
 
     it('should fail when requesting a read request when there is no token in the request header, and it will throw an error', async () => {
-        axiosVCR.mountCassette('./test/cassettes/principals/read/principal_read_fail_token.json', true);
         try {
-            await new Principal().getByID(principalID);
+            let scope = nock('/principals/asdasd', 'get', {}, 403, {
+                statusCode: 403,
+                status: false,
+                message: 'Forbidden',
+                errorCode: 3001
+            });
+            await new Principal().getByID('asdasd');
+            scope.done();
             throw 'should not reach this line, because the read request has no token, or it is malformed';
         } catch (e) {
             expect(e.message).to.be.equals('Forbidden');
         }
-        axiosVCR.ejectCassette('./test/cassettes/principals/read/principal_read_fail_token.json');
     });
 
     it('should return an object if the principal is found successfully while passing valid data', async () => {
-        axiosVCR.mountCassette('./test/cassettes/principals/read/principal_read.json');
+        nock('/identity/login', 'post', {
+            name: 'volcanic',
+            secret: 'volcanic!123',
+            dataset_id: '-1',
+            audience: '["volcanic"]'
+        }, 200, {
+            response: {
+                response: {
+                    token: 'eyJhbGciOiJFUzUxMiIsInR5cCI6IkpXVCIsImtpZCI6IjljYjg1YTc3YTllNWU0MTU3ODMyYTFlYTgzOTI3MDZhIn0.eyJleHAiOjE1NzI0OTYzNDIsInN1YiI6InVzZXI6Ly9zYW5kYm94Ly0xLzEvMS8yIiwibmJmIjoxNTcyNDkyNzQyLCJhdWRpZW5jZSI6WyJrcmFrYXRvYWV1IiwiLSJdLCJpYXQiOjE1NzI0OTI3NDIsImlzcyI6InZvbGNhbmljX2F1dGhfc2VydmljZV9hcDIifQ.AIIsVxwqsYWg3DqusQhC8qeBbIX22Rk6fZHwY2iNgnU-ghOJDmK9QNMZbqJDul5hqTXfFyB7HVw0SBXjivPtFunDAOytU-JupKTl7qgveRiU0oVMdtrtEI7iSNXS30p2ulEu0bumUjibTEW4oig0K4LJYoNxht_rPosOx_NPqCxp1ljB'
+                }
+            },
+            status: 200
+        });
+        nock('/principals/volcanic', 'get', {}, 200, {
+            response: {
+                secure_id: 'volcanic',
+                id: 1,
+                name: 'volcanic-principal',
+                dataset_id: '-1',
+                last_active_date: null,
+                login_attempts: 0,
+                active: true,
+                created_at: '2019-10-31T04:32:57.678Z',
+                updated_at: '2019-10-31T04:32:57.678Z',
+                status: true
+            }
+        });
         let read = await principal.withAuth().getByID('volcanic');
-        axiosVCR.ejectCassette('./test/cassettes/principals/read/principal_read.json');
         expect(read.id).to.exist;
     });
-
 });
--- test/principal/03_update.js
+++ test/principal/03_update.js
@@ -1,51 +1,95 @@
 const chai = require('chai'),
     chaiAsPromised = require('chai-as-promised'),
     sorted = require('chai-sorted'),
-    axiosVCR = require('axios-vcr'),
+    { nock } = require('../../src/helpers'),
     expect = chai.expect;
 chai.use(chaiAsPromised);
 chai.use(sorted);
 
 const Principal = require('../../v1').Principal;
-let currentTimestampSecond = '111',
-    tempPrincipalName = 'principal-test-one',
-    tempDataSetID = currentTimestampSecond,
-    principalID = null;
 describe('Principal updates', async () => {
     let principal = new Principal();
-    before(async () => {
-        axiosVCR.mountCassette('./test/cassettes/main_ops/principal/principal_update_test.json');
-        let create = await principal.withAuth().create(tempPrincipalName, tempDataSetID);
-        principalID = create.secure_id;
-        axiosVCR.ejectCassette('./test/cassettes/main_ops/principal/principal_update_test.json');
-    });
     //update principal
     it('upon principal update, the request should not be completed if there is no authorization token in the request header, and it will throw an error', async () => {
-        axiosVCR.mountCassette('./test/cassettes/principals/update/principal_update_fail.json', true);
         try {
-            await new Principal().update(principalID, 'new name', 12);
+            nock('/identity/login', 'post', {
+                name: 'volcanic',
+                secret: 'volcanic!123',
+                dataset_id: '-1',
+                audience: '["volcanic"]'
+            }, 200, {
+                response: {
+                    response: {
+                        token: 'eyJhbGciOiJFUzUxMiIsInR5cCI6IkpXVCIsImtpZCI6IjljYjg1YTc3YTllNWU0MTU3ODMyYTFlYTgzOTI3MDZhIn0.eyJleHAiOjE1NzI0OTYzNDIsInN1YiI6InVzZXI6Ly9zYW5kYm94Ly0xLzEvMS8yIiwibmJmIjoxNTcyNDkyNzQyLCJhdWRpZW5jZSI6WyJrcmFrYXRvYWV1IiwiLSJdLCJpYXQiOjE1NzI0OTI3NDIsImlzcyI6InZvbGNhbmljX2F1dGhfc2VydmljZV9hcDIifQ.AIIsVxwqsYWg3DqusQhC8qeBbIX22Rk6fZHwY2iNgnU-ghOJDmK9QNMZbqJDul5hqTXfFyB7HVw0SBXjivPtFunDAOytU-JupKTl7qgveRiU0oVMdtrtEI7iSNXS30p2ulEu0bumUjibTEW4oig0K4LJYoNxht_rPosOx_NPqCxp1ljB'
+                    }
+                },
+                status: 200
+            });
+            let scope = nock('/principals/334e5b1dd2', 'post', { name: 'new name', }, 403, {
+                message: 'Forbidden', errorCode: 3001
+            });
+            await new Principal().update('334e5b1dd2', 'new name');
+            scope.done();
             throw 'should not read this line because the update request has no token, or it is malformed';
         } catch (e) {
             expect(e.message).to.exist;
         }
-        axiosVCR.ejectCassette('./test/cassettes/principals/update/principal_update_fail.json');
     });
 
     it('should not update a principal that does not exist hence an error is thrown', async () => {
-        axiosVCR.mountCassette('./test/cassettes/principals/update/principal_update_fail_duplicate.json', true);
         try {
-            await principal.update(12, 'new name');
+            nock('/identity/login', 'post', {
+                name: 'volcanic',
+                secret: 'volcanic!123',
+                dataset_id: '-1',
+                audience: '["volcanic"]'
+            }, 200, {
+                response: {
+                    response: {
+                        token: 'eyJhbGciOiJFUzUxMiIsInR5cCI6IkpXVCIsImtpZCI6IjljYjg1YTc3YTllNWU0MTU3ODMyYTFlYTgzOTI3MDZhIn0.eyJleHAiOjE1NzI0OTYzNDIsInN1YiI6InVzZXI6Ly9zYW5kYm94Ly0xLzEvMS8yIiwibmJmIjoxNTcyNDkyNzQyLCJhdWRpZW5jZSI6WyJrcmFrYXRvYWV1IiwiLSJdLCJpYXQiOjE1NzI0OTI3NDIsImlzcyI6InZvbGNhbmljX2F1dGhfc2VydmljZV9hcDIifQ.AIIsVxwqsYWg3DqusQhC8qeBbIX22Rk6fZHwY2iNgnU-ghOJDmK9QNMZbqJDul5hqTXfFyB7HVw0SBXjivPtFunDAOytU-JupKTl7qgveRiU0oVMdtrtEI7iSNXS30p2ulEu0bumUjibTEW4oig0K4LJYoNxht_rPosOx_NPqCxp1ljB'
+                    }
+                },
+                status: 200
+            });
+            let scope = nock('/principals/12', 'post', { name: 'new name', }, 404, {
+                message: 'Principal does not exist', errorCode: 2002
+            });
+            await principal.withAuth().update(12, 'new name');
+            scope.done();
             throw 'should not reach this line because the principal requested does not exist';
         } catch (e) {
-            expect(e.message).to.exist;
+            expect(e.errorCode).to.equal(2002);
+            expect(e.message).to.equal('Principal does not exist');
         }
-        axiosVCR.ejectCassette('./test/cassettes/principals/update/principal_update_fail_duplicate.json');
     });
 
     it('should be a success when the principal is updated, thus it will return an object carrying the new attributes for the principal', async () => {
-        axiosVCR.mountCassette('./test/cassettes/principals/update/principal_update.json');
-        let update = await principal.withAuth().update(principalID, 'new name', 12);
+        nock('/identity/login', 'post', {
+            name: 'volcanic',
+            secret: 'volcanic!123',
+            dataset_id: '-1',
+            audience: '["volcanic"]'
+        }, 200, {
+            response: {
+                response: {
+                    token: 'eyJhbGciOiJFUzUxMiIsInR5cCI6IkpXVCIsImtpZCI6IjljYjg1YTc3YTllNWU0MTU3ODMyYTFlYTgzOTI3MDZhIn0.eyJleHAiOjE1NzI0OTYzNDIsInN1YiI6InVzZXI6Ly9zYW5kYm94Ly0xLzEvMS8yIiwibmJmIjoxNTcyNDkyNzQyLCJhdWRpZW5jZSI6WyJrcmFrYXRvYWV1IiwiLSJdLCJpYXQiOjE1NzI0OTI3NDIsImlzcyI6InZvbGNhbmljX2F1dGhfc2VydmljZV9hcDIifQ.AIIsVxwqsYWg3DqusQhC8qeBbIX22Rk6fZHwY2iNgnU-ghOJDmK9QNMZbqJDul5hqTXfFyB7HVw0SBXjivPtFunDAOytU-JupKTl7qgveRiU0oVMdtrtEI7iSNXS30p2ulEu0bumUjibTEW4oig0K4LJYoNxht_rPosOx_NPqCxp1ljB'
+                }
+            },
+            status: 200
+        });
+        nock('/principals/334e5b1dd2', 'post', { name: 'new name', }, 200, {
+            response: {
+                id: '334e5b1dd2',
+                name: 'n******e',
+                dataset_id: '111',
+                last_active_date: null,
+                login_attempts: 0,
+                active: true,
+                created_at: '2019-10-31T04:33:06.089Z',
+                updated_at: '2019-10-31T08:36:16.906Z'
+            }
+        });
+        let update = await principal.withAuth().update('334e5b1dd2', 'new name');
         expect(update.dataset_id).to.exist;
-        axiosVCR.ejectCassette('./test/cassettes/principals/update/principal_update.json');
     });
 });
