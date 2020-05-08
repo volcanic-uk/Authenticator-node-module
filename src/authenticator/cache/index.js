da393194debfb0a804cad11e4c72a7b4ffd07508
Ali Dalal <ali.dalal92@gmail.com>
2020-05-08 12:27:54 +0800

    increase default cache duration to 3600 seconds = 1 hour

--- src/authenticator/cache/index.js
+++ src/authenticator/cache/index.js
@@ -2,7 +2,7 @@ const NodeCache = require('node-cache');
 const config = require('../../../config');
 const cache = new NodeCache();
 
-exports.put = (key, value, duration = 60) => {
+exports.put = (key, value, duration = 3600) => {
     if (config.cache.enableCaching) {
         return cache.set(key, value, duration);
     }
