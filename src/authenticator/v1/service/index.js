fe2ca2cb28669e318090963da4f552187193f9f3
nisalikularatne <nisalikularatne@yahoo.com>
2020-06-09 10:38:55 +0800

    name param added to services

--- src/authenticator/v1/service/index.js
+++ src/authenticator/v1/service/index.js
@@ -20,8 +20,8 @@ class Service extends V1Base {
         return super.fetch('get', `services/${name}`, null);
     }
 
-    async getServices(page = '', pageSize = '', sort = 'id', order = 'asc', ids) {
-        return super.fetch('get', `services?page=${page}&page_size=${pageSize}&sort=${sort}&order=${order}&ids=${ids}`, null);
+    async getServices(page = '', pageSize = '', name = 'auth', sort = 'id', order = 'asc', ids) {
+        return super.fetch('get', `services?page=${page}&page_size=${pageSize}&name=${name}&sort=${sort}&order=${order}&ids=${ids}`, null);
     }
 
     async update(id, name) {
