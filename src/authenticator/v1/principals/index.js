0d3c23d03eec84fe58c50425b19df1d0ba4265a3
Nisali Sanara Kularatne <nisalikularatne@yahoo.com>
2020-06-16 16:21:15 +0800

    get identities in principals modified (#46)

--- src/authenticator/v1/principals/index.js
+++ src/authenticator/v1/principals/index.js
@@ -111,7 +111,6 @@ class Principal extends V1Base {
     async activatePrincipal(id) {
         return super.fetch('post', `principals/${id}/activate`, null, {});
     }
-
     /**
      *
      * @param page
@@ -125,8 +124,8 @@ class Principal extends V1Base {
      * @param id the id of the principal
      * @returns {Promise<*|{status}|undefined>}
      */
-    async getIdentities({ page = 1, page_size = 10, query = null, name = null, source = null, dataset_id = null, sort = 'id', order = 'asc', id = null }) {
-        return super.fetch('post', `principals/${id}/identities?query=${query}&dataset_id=${dataset_id}&page=${page}&${page_size}&sort=${sort}&order=${order}&name=${name}&source=${source}`, null, {});
+    async getIdentities({ page = 1, page_size = 10, query = '', name = '', source = '', dataset_id = null, sort = 'id', order = 'asc', id = null }) {
+        return super.fetch('get', `principals/${id}/identities?query=${query}&dataset_id=${dataset_id}&page=${page}&${page_size}&sort=${sort}&order=${order}&name=${name}&source=${source}`, null, {});
     }
 
     /**
